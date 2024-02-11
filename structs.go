4203536b6cf3311de78ff5b51b3b9c95af567c09
Caleb Sideras <caleb.sideras@outlook.com>
2024-02-11 14:55:13 -0500

    Initial Commit

---
+++ structs.go
@@ -0,0 +1,24 @@
+package temporary
+
+import (
+	"github.com/a-h/templ"
+)
+
+type HandleType int64
+
+const (
+	DefaultHandler HandleType = iota // no params
+	ResReqHandler                    // Response and Request
+)
+
+type Handler struct {
+	Path    string
+	Handler interface{}
+	HandleType
+}
+type RenderDefault struct {
+	Path    string
+	Handler func() templ.Component
+}
+
+type IndexDefaultFunc func() templ.Component
